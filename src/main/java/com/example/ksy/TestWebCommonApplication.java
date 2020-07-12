ce1593cc53f2467739d88a8e8ff28e655d58338b
KSY <kox501@naver.com>
2020-07-13 00:04:26 +0900

    Spring boot multi module Test

--- src/main/java/com/example/ksy/TestWebCommonApplication.java
+++ src/main/java/com/example/ksy/TestWebCommonApplication.java
@@ -1,13 +1,12 @@
-package com.example.testwebcommon;
+package com.example.ksy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestWebCommonApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TestWebCommonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebCommonApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/ksy/service/CommonService.java
@@ -0,0 +1,10 @@
+package com.example.ksy.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommonService {
+    public String test () {
+        return "test-web-common";
+    }
+}
